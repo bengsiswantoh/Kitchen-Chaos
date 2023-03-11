56057df9008c432dce048df5840e76bc5981e5e0
bengsiswanto <bengsiswantoh@gmail.com>
2023-03-12 01:33:13 +0700

    interact alternate

--- Assets/Scripts/BaseCounter.cs
+++ Assets/Scripts/BaseCounter.cs
@@ -7,6 +7,7 @@ public class BaseCounter : MonoBehaviour, IKitchenObjectParent
   private KitchenObject kitchenObject;
 
   public virtual void Interact(Player player) { Debug.LogError("BaseCounter.Interact();"); }
+  public virtual void InteractAlternate(Player player) { Debug.LogError("BaseCounter.InteractAlternate();"); }
 
   public Transform GetKitchenObjectFollowTransform() { return counterTopPoint; }
   public void SetKitchenObject(KitchenObject kitchenObject)
